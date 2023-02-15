f2271f23a4a9fb00eaea294c493dbcc92b60c4a5
Ivan Uvarov <uvarov_ivan@rambler.ru>
2023-02-15 15:36:47 +0300

    Finding weekend

---
+++ HomeWork2.2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+int Prompt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = Prompt("Введите цифру, обозначающую день недели");
+
+if (num > 0 && num < 6)
+{
+    System.Console.WriteLine($"День недели, который обозначается цифрой {num}, не является выходным.");
+}
+else if(num == 6 || num == 7)
+{
+    System.Console.WriteLine($"День недели, который обозначается цифрой {num}, является выходным.");
+}
+else
+{
+    System.Console.WriteLine($"Цифрой {num} день недели не обозначается.");
+}
