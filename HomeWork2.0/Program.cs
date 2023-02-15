311e0ff3f3e2d384714a95ea5c848a021c86cf7d
Ivan Uvarov <uvarov_ivan@rambler.ru>
2023-02-15 14:38:29 +0300

    Finding secont digit

---
+++ HomeWork2.0/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+int Prompt(string msg)
+{
+    System.Console.Write($"{msg} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = Prompt("Введите трёхзначное число  :");
+int secondDigit = 0;
+if (num <100 || num > 999)
+{
+    System.Console.WriteLine($" Число {num} не трёхзначное!!!");
+}
+else
+{
+secondDigit = (num / 10) % 10;
+System.Console.WriteLine($"Второй цифрой числа {num} является {secondDigit}");
+}
+
